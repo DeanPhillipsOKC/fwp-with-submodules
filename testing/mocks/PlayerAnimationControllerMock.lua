b79681f35663fd4c18d1cedf2d8f3031a1756619
DeanPhillipsOKC <DeanPhillipsOKC@gmail.com>
2019-01-06 13:04:45 -0600

    Created test cases for the player entity PlayAnimation, and StopAnimation methods

--- testing/mocks/PlayerAnimationControllerMock.lua
+++ testing/mocks/PlayerAnimationControllerMock.lua
@@ -2,10 +2,20 @@ local PlayerAnimationControllerMock = {}
 
 function PlayerAnimationControllerMock.new(player)
     local pacm = {
-        PlayerName = player.Name
+        PlayerName = player.Name,
+        AnimationsPlayed = {},
+        AnimationsStopped = {}
     }
     setmetatable(pacm, PlayerAnimationControllerMock)
     return pacm
+end
+
+function PlayerAnimationControllerMock:Play(animationName)
+    self.AnimationsPlayed[animationName] = true
+end
+
+function PlayerAnimationControllerMock:Stop(animationName)
+    self.AnimationsStopped[animationName] = true
 end
 
 PlayerAnimationControllerMock.__index = PlayerAnimationControllerMock
