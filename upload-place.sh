126de016aa3ed96a4bcf521d1c9dd8ee526c20e5
DeanPhillipsOKC <DeanPhillipsOKC@gmail.com>
2019-01-27 14:03:29 -0600

    Hopefully fixed the CI/CD upload script

--- upload-place.sh
+++ upload-place.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-curl -X POST -d @FishingWithPets.rbxlx -H "COOKIE: .ROBLOSECURITY=$ROBLOXSECURITY" -H "USER_AGENT: Roblox/WinInet" -H "Requester: Client" -H "CONTENT_TYPE: application/xml" -H "ACCEPT: application/json" "https://data.roblox.com/Data/Upload.ashx?assetid=2751232980"
+PLACE_ID="2751232980"
 
-#    let url = format!("", options.asset_id);
+RESULT=$(curl -v -X POST -d @FishingWithPets.rbxlx \
+    -H "COOKIE: .ROBLOSECURITY=$ROBLOXSECURITY" \
+    -H "USER-AGENT: Roblox/WinInet" \
+    -H "Requester: Client" \
+    -H "CONTENT-TYPE: application/xml" \
+    -H "ACCEPT: application/json" \
+    "https://data.roblox.com/Data/Upload.ashx?assetid=$PLACE_ID")
 
-# -H "Accept-Charset: utf-8"
-
-#    let client = reqwest::Client::new();
-#    let mut response = client.post(&url)
-#        .header(COOKIE, format!(".ROBLOSECURITY={}", &options.security_cookie))
-#        .header(USER_AGENT, "Roblox/WinInet")
-#        .header("Requester", "Client")
-#        .header(CONTENT_TYPE, "application/xml")
-#        .header(ACCEPT, "application/json")
-#        .body(contents)
-#        .send()?;
+if [ "$RESULT" != $PLACE_ID ]
+then
+    echo "$RESULT"
+    echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"
+    echo "# # #   E R R O R   U P L O A D I N G   T H E   P L A C E   F I L E !   # # #"
+    echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"
+    exit 100
+fi
