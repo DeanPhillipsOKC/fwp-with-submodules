dccd9ff29e670bf013d1ab4b06357474ed9466ca
DeanPhillipsOKC <DeanPhillipsOKC@gmail.com>
2019-02-02 18:08:08 -0600

    Created the fish bag rodux reducer

--- src/client/viewmodel/FishBagReducer/init.lua
+++ src/client/viewmodel/FishBagReducer/init.lua
@@ -1,14 +1,32 @@
 local function reducer(state, action)
     
-    if action.type == "caughtfish" then
-        return {
-            Name = action.fishName,
-            ImageId = action.fishImageId,
-            TimeLastFishWasCaught = action.timeLastFishWasCaught
-        }
+    local dependencies = require(script.Dependencies).Get()
+    
+    if action.type == "fishBagContentsChanged" then
+        assert(action.FishBag ~= nil, "Fish bag contents changed, but were not sent with the event dispatcher.")
+        assert(action.FishBag.Total ~= nil, "Fish bag contents changed, but the event dispatcher did not send a new total.")
+        assert(action.FishBag.Contents ~= nil, "Fish bag contents changed, but the event dispatcher did not send new contents.")
+
+        local totalFishInBag = action.FishBag.Total
+        local bagContents = action.FishBag.Contents
+
+        local newState = {}
+        newState.Total = totalFishInBag
+        newState.Contents = {}
+
+        for k,v in pairs(bagContents) do
+            newState.Contents[k] = {}
+            newState.Contents[k].ImageAssetId = dependencies.FishImageService.GetAssetIdFromName(k)
+            newState.Contents[k].Total = v
+        end
+
+        return newState
     end
 
-    return state.CaughtFish or {}
+    return state.FishBag or {
+        Total = 0,
+        Contents = {}
+    }
 end
 
 return reducer
